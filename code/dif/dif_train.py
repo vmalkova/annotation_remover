ba953ed09b45047626bc1528a7af5166345fcf05
Vera Malkova <vmalkova343@gmail.com>
2024-08-14 15:54:49 +0100

    Empty cuda cache, set progress bar ephochs to 2.sf consistently

--- code/dif/dif_train.py
+++ code/dif/dif_train.py
@@ -8,49 +8,33 @@ from diffusers import DDPMScheduler, DDPMPipeline
 from diffusers.optimization import get_cosine_schedule_with_warmup
 from tqdm.auto import tqdm
 from PIL import Image
-from dif_model import ButterflyModel
+from dif_model import UNET2D
 from dataclasses import dataclass
 from augmentation import ImageFolder
 
 
-NUM_BATCHES = 20
-NUM_WORKERS = 24
+NUM_BATCHES = 100
+NUM_WORKERS = 12
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+torch.cuda.empty_cache()
 
 @dataclass
 class TrainingConfig:
     image_size = 128
-    train_batch_size = 16
-    eval_batch_size = 16
-    num_epochs = 6
+    train_batch_size = 400
+    eval_batch_size = 80
+    num_epochs = 30
     gradient_accumulation_steps = 1
     learning_rate = 1e-4
-    lr_warmup_steps = 100 # 500
-    save_image_epochs = 2
-    save_model_epochs = 4
+    lr_warmup_steps = 500
+    save_image_epochs = 5
+    save_model_epochs = 10
     mixed_precision = 'fp16'  # `no` for float32, `fp16` for automatic mixed precision
     output_dir = 'result'  # the model name locally and on the HF Hub
     load_model = True
     seed = 0
     dataset_name  = "rocks"
 
-
-def print_to_tensorboard(step, prediction):
-    EXAMPLES = min(768//config.image_size,8)
-    with torch.no_grad():
-        for real, annotated in train_dataloader:
-            real = real.to(DEVICE)
-            annotated = annotated.to(DEVICE)
-            print(prediction.type())
-
-            # img_grid_real = torchvision.utils.make_grid(real[:EXAMPLES], normalize=True)
-            # img_grid_fake = torchvision.utils.make_grid(prediction[:EXAMPLES], normalize=True)
-            # img_grid_annotated = torchvision.utils.make_grid(annotated[:EXAMPLES], normalize=True)
-
-            # writer.add_image(f"real", img_grid_real, global_step=step)
-            # writer.add_image(f"fake", img_grid_fake, global_step=step)
-            # writer.add_image(f"annotated", img_grid_annotated, global_step=step)
-    return
 
 def make_grid(images, rows, cols):
     w, h = images[0].size
@@ -66,9 +50,11 @@ def evaluate(config, epoch, pipeline):
     ).images
     image_grid = make_grid(images, rows=4, cols=4)
     test_dir = os.path.join(config.output_dir, "samples")
+    if os.path.exists(test_dir):
+        for file in os.listdir(test_dir):
+            os.remove(os.path.join(test_dir, file))
     os.makedirs(test_dir, exist_ok=True)
     image_grid.save(f"{test_dir}/{epoch:04d}.png")
-    print_to_tensorboard(step=epoch, prediction=images)
 
 def train_loop(config, noise_scheduler, train_dataloader):
     global model, optimizer, lr_scheduler
@@ -90,7 +76,7 @@ def train_loop(config, noise_scheduler, train_dataloader):
     # train
     for epoch in range(config.num_epochs):
         progress_bar = tqdm(train_dataloader)
-        progress_bar.set_description(f"Epoch {epoch}")
+        progress_bar.set_description(f"Epoch {str(epoch).zfill(2)}")
         for i, (clean, _) in enumerate(train_dataloader):
             clean_images = clean.to(DEVICE)
             noise = torch.randn(clean_images.shape).to(DEVICE)
@@ -129,10 +115,7 @@ if __name__ == "__main__":
     dataset = ImageFolder(config.image_size, config.train_batch_size*NUM_BATCHES)
     train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, num_workers=NUM_WORKERS, shuffle=True)
     noise_scheduler = DDPMScheduler(num_train_timesteps=1000)
-    model = ButterflyModel(config)
-    if config.load_model:
-        model = model.from_pretrained(f"{config.output_dir}/unet")
-    model = ButterflyModel(config)
+    model = UNET2D(config)
     optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)
     lr_scheduler = get_cosine_schedule_with_warmup(
         optimizer=optimizer,
