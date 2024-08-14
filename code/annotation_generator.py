6b7a69b67611df93e97442e9db6a28b6e0fe7077
Vera Malkova <vmalkova343@gmail.com>
2024-08-14 15:22:09 +0100

    Split annotation generator into 2 classes, so 2 files (one for generating and saving matching images and the other for loading them from folders and changing folder size)

--- code/annotation_generator.py
+++ code/annotation_generator.py
@@ -2,7 +2,6 @@ import os
 from glob import glob
 import random
 from drawing import Draw
-from PIL import Image
 
 CROPPED_DIR, MATCHING_DIR = '../images/cropped', '../images/matching'
 
@@ -65,40 +64,9 @@ class AnnotationGenerator():
       cropped_images.extend(selected_images)
     return cropped_images
   
-  def get_matching_images(self, total_amount=None):
-    if total_amount == None: total_amount = self.num_matching_images()
-    matching_images = [] # list of path pairs
-    matching_dirs = [dir for dir in os.listdir(MATCHING_DIR) if os.path.isdir(f"{MATCHING_DIR}/{dir}")]
-    while len(matching_images) < total_amount:
-      selected_dir = matching_dirs.pop(random.randint(0, len(matching_dirs)-1))
-      len_dir = len(os.listdir(f'{MATCHING_DIR}/{selected_dir}/clean'))
-      imgs_left = total_amount - len(matching_images)
-      selected_images = random.sample(glob(f'{MATCHING_DIR}/{selected_dir}/clean/*.jpg'), min(len_dir, imgs_left))
-      for clean_img in selected_images:
-        pair = [clean_img]
-        annot_img = clean_img.replace("clean", "annotated")
-        if os.path.exists(annot_img):
-          pair.append(annot_img)
-          matching_images.append(pair)
-          for img_path in pair:
-            try:
-              Image.open(img_path).load()
-            except Exception as e:
-              print(f"Corrupted image: {img_path}")
-              for img in pair:
-                os.remove(img)
-        else:
-          print(f"Missing annotated image for {clean_img}")
-          # delete clean image if annotated image is missing
-          os.remove(clean_img)
-    return matching_images
-  
   def num_cropped_images(self):
     return sum([len(os.listdir(f'{CROPPED_DIR}/{dir}')) for dir in os.listdir(CROPPED_DIR) if os.path.isdir(f'{CROPPED_DIR}/{dir}')])
-  
-  def num_matching_images(self):
-    return sum([len(os.listdir(f'{MATCHING_DIR}/{dir}/clean')) for dir in os.listdir(MATCHING_DIR) if os.path.isdir(f'{MATCHING_DIR}/{dir}')])
-  
+
   def save_image(self, drawing:Draw, folder_type):
     folder = f'{MATCHING_DIR}/matching {self.save_dir}/{folder_type}'
     if not os.path.exists(folder):
@@ -109,25 +77,13 @@ class AnnotationGenerator():
       if os.path.exists(f"{folder}/{img_name}"): continue
       else:
         drawing.save(f"{folder}/{img_name}")
-        return
+        break
     return
 
   def save_matching(self, num_images):
     for img_path in self.get_cropped_images(num_images):
       self.save_image(Draw(path=img_path), "clean")
       self.save_image(Draw(img=self.annotate(path=img_path)), "annotated")
-    return
-  
-  def remove_matching(self, num_images):
-    for folder_type in ["clean", "annotated"]:
-      folder = f'{MATCHING_DIR}/matching {self.save_dir}/{folder_type}'
-      if os.path.exists(folder):
-        files = os.listdir(folder)
-        files.sort()
-        if files != []:
-          for _ in range(num_images):
-            file_to_remove = files.pop()
-            os.remove(f'{folder}/{file_to_remove}')
     return
 
   def switch_save_folder(self, save_dir=None):
@@ -139,52 +95,9 @@ class AnnotationGenerator():
           break
     self.save_dir = save_dir
     return
-  
-  def folder_size(self, folder_num=None):
-    if folder_num == None : folder_num = self.save_dir
-    folder = f'{MATCHING_DIR}/matching {folder_num}/clean'
-    size = 0
-    if os.path.exists(folder): size = len(os.listdir(folder))
-    return size
-  
-  def set_num_folders(self, num_folders):
-    # remove extra folders
-    for i in range(num_folders, len(os.listdir(MATCHING_DIR))):
-      if os.path.exists(f'{MATCHING_DIR}/matching {i}'):
-        # empty folder before removing it
-        for item in os.listdir(f'{MATCHING_DIR}/matching {i}'):
-          if os.path.isdir(f'{MATCHING_DIR}/matching {i}/{item}'):
-            for file in os.listdir(f'{MATCHING_DIR}/matching {i}/{item}'):
-              os.remove(f'{MATCHING_DIR}/matching {i}/{item}/{file}')
-            os.rmdir(f'{MATCHING_DIR}/matching {i}/{item}')
-          else:
-            os.remove(f'{MATCHING_DIR}/matching {i}/{item}')
-        os.rmdir(f'{MATCHING_DIR}/matching {i}')
-    # add missing folders
-    for i in range(len(os.listdir(MATCHING_DIR)), num_folders):
-      if not os.path.exists(f'{MATCHING_DIR}/matching {i}'):
-        os.makedirs(f'{MATCHING_DIR}/matching {i}')
-    return
-  
-  def set_folder_sizes(self, num_folders, folder_size):
-    for folder_num in range(num_folders):
-      self.switch_save_folder(folder_num)
-      current_folder_size = self.folder_size()
-      print(f"Folder {folder_num} size: {self.folder_size()}")
-      if current_folder_size < folder_size:
-        self.save_matching(folder_size-current_folder_size)
-        print(f"Increased to size: {self.folder_size()}")
-      elif current_folder_size > folder_size:
-        self.remove_matching(current_folder_size-folder_size)
-        print(f"Decreased to size: {self.folder_size()}")
-    return
 
 
 if __name__ == "__main__":
   ag = AnnotationGenerator()
-  num_folders, folder_size = 30, 500
-  print("Num folders:", num_folders)
-  print("Folder size:", folder_size)
-  ag.set_folder_sizes(num_folders, folder_size)
-  print("Matching pairs:", ag.num_matching_images())
-  print("Clean images:", ag.num_cropped_images())
+  ag.switch_save_folder()
+  ag.save_matching(10)
---
+++ code/image_loader.py
@@ -0,0 +1,114 @@
+import os
+import random
+import glob
+from PIL import Image
+from annotation_generator import AnnotationGenerator
+
+CROPPED_DIR, MATCHING_DIR = '../images/cropped', '../images/matching'
+
+
+class ImageLoader(AnnotationGenerator):
+  def __init__(self):
+    super().__init__()
+    return
+  
+  def get_matching_images(self, total_amount=None):
+    if total_amount == None: total_amount = self.num_matching_images()
+    matching_images = [] # list of path pairs
+    matching_dirs = [dir for dir in os.listdir(MATCHING_DIR) if os.path.isdir(f"{MATCHING_DIR}/{dir}")]
+    while len(matching_images) < total_amount:
+      selected_dir = matching_dirs.pop(random.randint(0, len(matching_dirs)-1))
+      len_dir = len(os.listdir(f'{MATCHING_DIR}/{selected_dir}/clean'))
+      imgs_left = total_amount - len(matching_images)
+      selected_images = random.sample(glob(f'{MATCHING_DIR}/{selected_dir}/clean/*.jpg'), min(len_dir, imgs_left))
+      for clean_img in selected_images:
+        pair = [clean_img]
+        annot_img = clean_img.replace("clean", "annotated")
+        if os.path.exists(annot_img):
+          pair.append(annot_img)
+          matching_images.append(pair)
+          for img_path in pair:
+            try:
+              Image.open(img_path).load()
+            except Exception as e:
+              print(f"Corrupted image: {img_path}")
+              for img in pair:
+                os.remove(img)
+        else:
+          print(f"Missing annotated image for {clean_img}")
+          os.remove(clean_img)
+    return matching_images
+  
+  def num_matching_images(self):
+    total = 0
+    for type in ["clean", "annotated"]:
+      total += sum([len(os.listdir(f'{MATCHING_DIR}/{dir}/{type}')) for dir in os.listdir(MATCHING_DIR) if os.path.isdir(f'{MATCHING_DIR}/{dir}')])
+    return total
+  
+  def remove_matching(self, new_num_images):
+    for folder_type in ["clean", "annotated"]:
+      folder = f'{MATCHING_DIR}/matching {self.save_dir}/{folder_type}'
+      # delete images in folder
+      for img_path in os.listdir(folder):
+        os.remove(f'{folder}/{img_path}')
+    # add images to folder
+    self.save_matching(new_num_images)
+    return
+  
+  def folder_size(self, folder_num=None):
+    if folder_num == None : folder_num = self.save_dir
+    matching_folder = f'{MATCHING_DIR}/matching {folder_num}'
+    size = 0
+    if os.path.exists(f'{matching_folder}/clean'):
+      print("clean:", len(os.listdir(f'{matching_folder}/clean')))
+      clean_size = len(os.listdir(f'{matching_folder}/clean'))
+    if os.path.exists(f'{matching_folder}/annotated'):
+      print("annotated:", len(os.listdir(f'{matching_folder}/annotated')))
+      annot_size = len(os.listdir(f'{matching_folder}/annotated'))
+    size = max(clean_size, annot_size)
+    return size
+  
+  def set_num_folders(self, num_folders):
+    # remove extra folders
+    for i in range(num_folders, len(os.listdir(MATCHING_DIR))):
+      folder = f'{MATCHING_DIR}/matching {i}'
+      if os.path.exists(folder):
+        # empty folder before removing it
+        for item in os.listdir(folder):
+          if os.path.isdir(f'{folder}/{item}'):
+            for file in os.listdir(f'{folder}/{item}'):
+              os.remove(f'{folder}/{item}/{file}')
+            os.rmdir(f'{folder}/{item}')
+          else:
+            os.remove(f'{folder}/{item}')
+        os.rmdir(folder)
+    # add missing folders
+    for i in range(len(os.listdir(MATCHING_DIR)), num_folders):
+      folder = f'{MATCHING_DIR}/matching {i}'
+      if not os.path.exists(folder):
+        os.makedirs(folder)
+    return
+  
+  def set_folder_sizes(self, folder_size):
+    for folder_num, _ in enumerate(os.listdir(MATCHING_DIR)):
+      self.switch_save_folder(folder_num)
+      current_folder_size = self.folder_size()
+      print(f"Folder {folder_num} size: {current_folder_size}")
+      if current_folder_size < folder_size:
+        self.save_matching(folder_size-current_folder_size)
+        print(f"Increased to size: {self.folder_size()}")
+      elif current_folder_size > folder_size:
+        self.remove_matching(folder_size)
+        print(f"Decreased to size: {self.folder_size()}")
+    return
+
+
+if __name__ == "__main__":
+  il = ImageLoader()
+  num_folders, folder_size = 268, 500
+  print("Num folders:", num_folders)
+  print("Folder size:", folder_size)
+  il.set_num_folders(num_folders)
+  il.set_folder_sizes(folder_size)
+  print("Matching pairs:", il.num_matching_images())
+  print("Clean images:", il.num_cropped_images())
