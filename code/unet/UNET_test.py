dd6840e72c48b820e961676b9c3d1aca02b39477
Vera Malkova <vmalkova343@gmail.com>
2024-08-22 15:03:20 +0100

    Only clean image if it isn't corrupted and isn't already cleaned

--- code/unet/UNET_test.py
+++ code/unet/UNET_test.py
@@ -8,15 +8,14 @@ from torchvision import transforms
 from UNET_model import UNET
 
 
-IMAGE_SIZE, BATCH_SIZE = 256, 16
-results_dir = f"../results/size{IMAGE_SIZE}_{BATCH_SIZE}"
+sizes = [path.split("size")[1] for path in os.listdir("results") if "size" in path]
+image_size = int(sorted(sizes)[-1])
+results_dir = f"results/size{image_size}"
 if not os.path.exists(results_dir): models_saved = []
 else: models_saved = [int(x.split("unet_checkpoint")[1][:4]) for x in os.listdir(results_dir) if "unet_checkpoint" in x]
 model_to_load = 0 if models_saved==[] else sorted(models_saved)[-1]
-if torch.backends.mps.is_available(): device = torch.device("mps")
-elif torch.backends.cuda.is_built(): device = torch.device("cuda")
-else: device = torch.device("cpu")
-load_img_dir, save_img_dir = "../images/test/to_clean", "../images/test/done"
+device = torch.device("cpu")
+load_img_dir, save_img_dir = "images/test/to_clean", "images/test/done"
 
 
 class ImageFolder(nn.Module):
@@ -24,7 +23,15 @@ class ImageFolder(nn.Module):
         super(ImageFolder, self).__init__()
         self.size = img_size
         self.to_tensor = transforms.ToTensor()
-        self.images = [f"{load_img_dir}/{x}" for x in os.listdir(load_img_dir)]
+        self.images = []
+        for img in os.listdir(load_img_dir):
+            img_path = f"{load_img_dir}/{img}"
+            try:
+                Image.open(img_path).load()
+                if not any([img in name for name in os.listdir(save_img_dir)]):
+                    self.images.append(img_path)
+            except Exception:
+                print(f"Corrupted image: {img}")
         
     def __len__(self):
         return len(self.images)
@@ -34,7 +41,9 @@ class ImageFolder(nn.Module):
         resize = self.transformation(width, height)
         img = Image.open(self.images[i])
         new_img = resize(image=np.array(img))["image"]
-        return self.to_tensor(new_img)
+        new_img = self.to_tensor(new_img)
+        name = self.images[i].split("/")[-1]
+        return new_img, name
     
     def transformation(self, width, height):
         s1, s2, actual_size = int(self.size*3.14), self.size, min(width, height)
@@ -46,24 +55,23 @@ class ImageFolder(nn.Module):
 def run(model, dataloader):
     model.eval()
     with torch.no_grad():
-        for batch_idx, real in enumerate(dataloader):
+        for _, (real, name) in enumerate(dataloader):
             real = real.to(device)
             prediction = model(real)
             prediction = torch.sigmoid(prediction)
             for i, img in enumerate(prediction):
                 img = transforms.ToPILImage()(img)
-                img.save(f"{save_img_dir}/{images[batch_idx*BATCH_SIZE+i]}")
+                img.save(f"{save_img_dir}/clean {name[i]}")
     return
 
 
 if __name__ == "__main__":
-    if model_to_load == 0: print (f"Model (img_size: {IMAGE_SIZE}, batch_size: {BATCH_SIZE}) doesn't exist"); exit()
-    print (f"Model {model_to_load} loaded")
+    if model_to_load == 0: print (f"Model (size {image_size}) doesn't exist"); exit()
+    print (f"Model (size {image_size} v{model_to_load}) loaded")
     model = UNET(3, 3).to(device)
     load_path = f"{results_dir}/unet_checkpoint{str(model_to_load).zfill(4)}.pth.tar"
     model.load_state_dict(torch.load(load_path, map_location=device)["state_dict"])
-    images = os.listdir(load_img_dir)
-    dataset = ImageFolder(IMAGE_SIZE)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)
+    dataset = ImageFolder(image_size)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=False)
     run(model, dataloader)
     print ("Images saved")
